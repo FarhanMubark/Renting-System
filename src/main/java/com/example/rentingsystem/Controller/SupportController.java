52a66dcbcd85503d7c7741e0b0a9a0f55a7285ab
Abdulaziz Aljabri <abdulaziz.aljabri18@gmail.com>
2023-09-06 17:24:37 +0300

    model

---
+++ src/main/java/com/example/rentingsystem/Controller/SupportController.java
@@ -0,0 +1,39 @@
+package com.example.rentingsystem.Controller;
+
+import com.example.rentingsystem.Api.ApiResponse;
+import com.example.rentingsystem.Model.Support;
+import com.example.rentingsystem.Service.SupportSerivce;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/support")
+@RequiredArgsConstructor
+public class SupportController {
+
+    private final SupportSerivce supportSerivce;
+
+    @GetMapping("/")
+    public ResponseEntity getSupports(){
+        return ResponseEntity.status(HttpStatus.OK).body(supportSerivce.getSupports());
+    }
+    @PostMapping("/")
+    public ResponseEntity addSupport(@RequestBody @Valid Support support){
+        supportSerivce.addSupports(support);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("added"));
+    }
+    @DeleteMapping("/{supportId}")
+    public ResponseEntity removeSupport(@PathVariable Integer supportId){
+        supportSerivce.removeSupports(supportId);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("deleted"));
+    }
+    @PutMapping("/{supportId}")
+    public ResponseEntity updateSupport(@RequestBody @Valid Support support, @PathVariable Integer supportId){
+        supportSerivce.updateSupport(support,supportId);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("updated"));
+    }
+}
+
---
+++ src/main/java/com/example/rentingsystem/Service/ProductService.java
@@ -0,0 +1,47 @@
+package com.example.rentingsystem.Service;
+
+import com.example.rentingsystem.Api.ApiException;
+import com.example.rentingsystem.Model.Product;
+import com.example.rentingsystem.Repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    public void addProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void updateProduct(Product product,Integer productId){
+        Product product1 = productRepository.findProductById(productId);
+        if(product1 == null){
+            throw new ApiException("Product not found");
+        }
+        product1.setProductName(product.getProductName());
+        product1.setProductStatus(product.getProductStatus());
+        product1.setProductPrice(product.getProductPrice());
+        product1.setProductCategory(product.getProductCategory());
+        product1.setDuration(product.getDuration());
+        product1.setProductDescription(product.getProductDescription());
+        productRepository.save(product1);
+    }
+
+    public void removeProduct(Integer productId){
+        Product product1 = productRepository.findProductById(productId);
+        if(product1 == null){
+            throw new ApiException("Product not found");
+        }
+        productRepository.delete(product1);
+    }
+
+
+}
