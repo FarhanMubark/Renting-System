a68d7f0d04ba8cfee0038eadc0078113d87a305d
FarhanMubark <farhan2020sa@hotmail.com>
2023-09-11 10:37:16 +0300

    add product model  validation

--- src/main/java/com/example/rentingsystem/Model/Product.java
+++ src/main/java/com/example/rentingsystem/Model/Product.java
@@ -2,6 +2,8 @@ package com.example.rentingsystem.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,22 +23,36 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotEmpty(message = "Should Not be empty")
+    @Column(columnDefinition = "varchar(25) not null")
     private String productName;
 
+    @NotEmpty(message = "Should Not be empty")
+    @Column(columnDefinition = "varchar(25) not null")
     private String productNumber;
 
-
+    @NotEmpty(message = "Should Not be empty")
+    @Column(columnDefinition = "int not null")
     private Integer productPrice;
 
+    @NotEmpty(message = "Should Not be empty")
+    @Column(columnDefinition = "varchar(100) not null")
     private String productDescription;
 
+    @NotEmpty(message = "Should Not be empty")
+    @Column(columnDefinition = "varchar(25) not null")
     private String productCategory;
 
+    @NotEmpty(message = "Should Not be empty")
+    @Column(columnDefinition = "varchar(25) not null")
     private String productStatus;
 
     private LocalDateTime endDate;
 
+    @NotEmpty(message = "Should Not be empty")
+    @Column(columnDefinition = "int not null")
     private Integer quantity;
+
     private Integer review;
 
     @ManyToOne
@@ -46,7 +62,5 @@ public class Product {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy ="product")
     private Set<MyOrder> myOrderSet;
-
-
 
 }
