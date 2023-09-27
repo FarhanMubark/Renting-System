9d85c33cb5166edbf02a82d8937641d85acd525b
FarhanMubark <farhan2020sa@hotmail.com>
2023-09-27 19:07:08 +0300

    dockerize the system

--- src/main/java/com/example/rentingsystem/Controller/RenterController.java
+++ src/main/java/com/example/rentingsystem/Controller/RenterController.java
@@ -20,32 +20,36 @@ public class RenterController {
     private final RenterService renterService;
 
     @GetMapping("/get")
-    public ResponseEntity getRenters(){
+    public ResponseEntity getRenters() {
         return ResponseEntity.status(200).body(renterService.getRenters());
     }
 
     @PutMapping("/update-{renter_id}")
-    public ResponseEntity update(@PathVariable Integer renter_id , @RequestBody Renter renter){
-        renterService.update(renter_id,renter);
+    public ResponseEntity update(@PathVariable Integer renter_id, @RequestBody Renter renter) {
+        renterService.update(renter_id, renter);
         return ResponseEntity.status(200).body(new ApiResponse("Renter updated"));
     }
 
 
     @GetMapping("/search-{name}")
-    public ResponseEntity findRenterByName(@PathVariable String name){
+    public ResponseEntity findRenterByName(@PathVariable String name) {
         return ResponseEntity.status(200).body(renterService.getRenterByName(name));
 
+    }
+
     @GetMapping("/renter-info")
-    public ResponseEntity findRenterByName(@AuthenticationPrincipal User user){
+    public ResponseEntity findRenterByName(@AuthenticationPrincipal User user) {
         return ResponseEntity.status(200).body(renterService.getRenterByName(user.getRenter().getName()));
 
     }
 
 
     @PostMapping("/buy-product/{product_id}/{typeOfDay}/{quantity}/{duration}")
-    public ResponseEntity updateProduct(@AuthenticationPrincipal User user,@PathVariable Integer product_id,@PathVariable String typeOfDay,@PathVariable Integer quantity,@PathVariable Integer duration){
-        renterService.buyProduct(user.getRenter().getId(),product_id,typeOfDay,quantity,duration);
+    public ResponseEntity updateProduct(@AuthenticationPrincipal User user, @PathVariable Integer product_id, @PathVariable String typeOfDay, @PathVariable Integer quantity, @PathVariable Integer duration) {
+        renterService.buyProduct(user.getRenter().getId(), product_id, typeOfDay, quantity, duration);
         return ResponseEntity.status(200).body(new ApiResponse("product bought successfully"));
     }
-
 }
+
+
+
